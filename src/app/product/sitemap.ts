f58a9636fb6c3d078d39fe75437865fa38767658
sukhantharot <93711042+oatfazwaz@users.noreply.github.com>
2024-10-02 17:44:32 +0700

    updated: sitemap

---
+++ src/app/product/sitemap.ts
@@ -0,0 +1,13 @@
+import {MetadataRoute} from "next";
+import {TProduct} from "@/app/search/[[...slug]]/page";
+async function fetchProducts() {
+  const res = await fetch('https://sukhantharot.github.io/dummy-fake-json/product.json');
+  return res.json();
+}
+export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
+  const products = await fetchProducts()
+  return products.map((product:TProduct) => ({
+    url: `https://breathoftravel.vercel.app/product/${product.id}`,
+    lastModified: '2024-10-2',
+  }))
+}
